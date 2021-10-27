c04e2d4c7ca78381e674ed89ec4a7876b722061a
younglaecho <tks7205@gmail.com>
2021-10-27 22:06:19 +0900

    react todoapp optimize learn

--- todo-app3/src/App.js
+++ todo-app3/src/App.js
@@ -1,55 +1,89 @@
-import React, { useCallback, useRef, useState } from 'react'
+import React, { useCallback, useReducer, useRef, useState } from 'react'
 import TodoTemplate from './Components/TodoTemplate';
 import TodoInsert from './Components/TodoInsert';
 import TodoList from './Components/TodoList'
+import { MdCallToAction } from 'react-icons/md';
+
+function createBulkTodos () {
+  const array = []
+  for (let i = 1; i < 2500; i++) {
+    array.push({
+      id:i,
+      text: `할 일 ${i}`,
+      checked: false
+    })
+  }
+  return array
+}
+
+function todoReducer (todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo)
+    case 'REMOVE':
+      return todos.filter(todo=> todo.id!==action.id)
+    case 'TOGGLE':
+      return todos.map(todo=>
+        todo.id ===action.id? {...todo, checked:!todo.checked}: todo   
+      )
+    default:
+      return todos;
+  }
+}
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id:1,
-      text: '리액트의 기초알아보기',
-      checked: true,
-    },
-    {
-      id:2,
-      text: '컴포넌트 스타일링해 보기',
-      checked: true
-    },
-    {
-      id:3,
-      text: '일정 관리 앱 만들기',
-      checked: false
-    }
-  ])
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+  // const [todos, setTodos] = useState(createBulkTodos())
+  // const [todos, setTodos] = useState([
+  //   {
+  //     id:1,
+  //     text: '리액트의 기초알아보기',
+  //     checked: true,
+  //   },
+  //   {
+  //     id:2,
+  //     text: '컴포넌트 스타일링해 보기',
+  //     checked: true
+  //   },
+  //   {
+  //     id:3,
+  //     text: '일정 관리 앱 만들기',
+  //     checked: false
+  //   }
+  // ])
 
-  const nextId = useRef(4)
-  const onInsert = useCallback(
+  const nextId = useRef(2501)
+  const onInsert = useCallback(todos=>
     text => {
       const todo= {
         id:nextId.current,
         text,
         checked:false
       }
-    setTodos(todos.concat(todo))
+    // setTodos(todos.concat(todo))
+    dispatch({type:'INSERT', todo})
     nextId.current += 1
     },
-    [todos]
+    []
   )
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo=> todo.id!==id))
-    }, [todos]
+      // setTodos(todos=>todos.filter(todo=> todo.id!==id))
+      dispatch({type:'REMOVE', id})
+    }, []
   )
 
   const onToggle = useCallback(
     id => {
-      setTodos(
-        todos.map(todo=> 
-          todo.id === id ? {...todo, checked:!todo.checked} : todo,
-        )
-      )
-    }
+      // setTodos(todos => 
+      //   todos.map(todo=> 
+      //     todo.id === id ? {...todo, checked:!todo.checked} : todo,
+      //   )
+      // )
+      dispatch({type:"TOGGLE", id})
+    }, []
+
   )
 
   return (
--- todo-app3/src/Components/TodoListItem.js
+++ todo-app3/src/Components/TodoListItem.js
@@ -7,10 +7,10 @@ import {
 import cn from 'classname';
 import './TodoListItem.scss'
 
-const TodoListItem = ({todo, onRemove, onToggle}) => {
+const TodoListItem = ({todo, onRemove, onToggle, style}) => {
   const { id, text, checked } = todo
   return (
-  
+  <div className="TodoListItem-virtualized" style={style}>
     <div className="TodoListItem">
       <div className={cn('checkbox', {checked})} onClick={()=>onToggle(id)}>
         {checked? <MdCheckBox/>:<MdCheckBoxOutlineBlank/>}
@@ -20,7 +20,8 @@ const TodoListItem = ({todo, onRemove, onToggle}) => {
         <MdRemoveCircleOutline/>
       </div>
     </div>
+  </div>
   )
 }
 
-export default TodoListItem
+export default React.memo(TodoListItem)
