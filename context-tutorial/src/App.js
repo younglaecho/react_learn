f46595541375f68bf2763f53f11b1c3e22cb503b
younglaecho <tks7205@gmail.com>
2021-11-08 23:42:08 +0900

    ' d

---
+++ context-tutorial/src/App.js
@@ -0,0 +1,11 @@
+import ColorBox from "./components/ColorBox";
+
+function App() {
+  return (
+    <div >
+      <ColorBox></ColorBox>
+    </div>
+  );
+}
+
+export default App;
---
+++ context-tutorial/src/components/ColorBox.js
@@ -0,0 +1,22 @@
+import react from 'react'
+import ColorContext from '../contexts/color'
+
+const ColorBox = () => {
+  return (
+    <ColorContext.Consumer>
+      {value => (
+        <div 
+          style={{
+            width:'64px',
+            height:'64px',
+            background: value.color
+          }}  
+        >
+          
+        </div>
+      )}
+    </ColorContext.Consumer>
+  )
+}
+
+export default ColorBox
